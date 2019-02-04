91eed4ff2af096a3fa31b4b02c864b58565e3ad0
Romika13 <47132557+Romika13@users.noreply.github.com>
2019-02-05 00:13:30 +0530

    Add files via upload

---
+++ CSharp assignment/DepthFirstsearch.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R.DepthFirstSearch
+{
+    class DepthFirstsearch
+    {
+        public Node root =null;
+        public void inOrder(Node root)
+        {
+            if(root==null)
+            {
+                return;
+            }
+            else
+            {
+                inOrder(root.left);
+                Console.WriteLine(root.data);
+                inOrder(root.right);
+            }
+        }
+        public void preOrder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine(root.data);
+                preOrder(root.left);
+                preOrder(root.right);
+            }
+        }
+        public void postOrder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                postOrder(root.left);
+                postOrder(root.right);
+                Console.WriteLine(root.data);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DepthFirstsearch ob = new DepthFirstsearch();
+            try
+            {
+                ob.root = new Node(1);
+                ob.root.left = new Node(2);
+                ob.root.right = new Node(3);
+                ob.root.left.left = new Node(4);
+                ob.root.left.right = new Node(5);
+                ob.root.right.left = new Node(6);
+                Console.WriteLine("DFS InOrder traversal of binary tree is: ");
+                ob.inOrder(ob.root);
+                Console.WriteLine("DFS preorder traversal of binary tree is: ");
+                ob.preOrder(ob.root);
+                Console.WriteLine("DFS postOrder traversal of binary tree is: ");
+                ob.postOrder(ob.root);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Node
+    {
+
+        public int data;
+        public Node left, right;
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+
+
+
+
+    
+}
---
+++ CSharp assignment/breadthFirstSearch.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R.BreadthFirstSearch
+{
+    class breadthFirstSearch
+    {
+        private Node root = null;
+        void traverseLevelWise(Node root,int level)
+        {
+            if(root==null)
+            {
+                return;
+            }
+            else
+            {
+                if (level == 0)
+                    Console.WriteLine(root.data);
+                else
+                {
+                    traverseLevelWise(root.lChild, level - 1);
+                    traverseLevelWise(root.rChild, level - 1);
+                }
+            }
+        }
+
+        public int height(Node root)
+        {
+            if(root==null)
+            {
+                return 0;
+            }
+            else
+            {
+                int lheight = height(root.lChild);
+                int rheight = height(root.rChild);
+                if (lheight > rheight)
+                    return (lheight + 1);
+                else
+                    return (rheight + 1);
+
+            }
+        }
+        public void display(Node root)
+        {
+            int h= height(root);
+            for(int i =0; i<h;i++)
+            {
+                traverseLevelWise(root, i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            breadthFirstSearch ob = new breadthFirstSearch();
+            try
+            {
+                ob.root = new Node(23);
+                ob.root.lChild = new Node(28);
+
+                ob.root.rChild = new Node(31);
+
+                ob.root.lChild.lChild = new Node(42);
+
+                ob.root.lChild.rChild = new Node(53);
+
+                ob.root.rChild.lChild = new Node(68);
+
+                Console.WriteLine("BFS traversal of tree is: ");
+
+                ob.display(ob.root);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Node
+    {
+        public int data;
+        public Node lChild;
+        public Node rChild;
+        public Node(int item)
+        {
+            data = item;
+            lChild = rChild = null;
+        }
+    }
+}
